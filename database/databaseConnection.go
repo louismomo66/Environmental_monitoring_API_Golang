5c9db9b5c33d2223c56527b074ec34baee2e72de
Louis Kwezi <kwezi.louishg@gmail.com>
2023-03-15 17:53:19 +0300

    data logger

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -6,10 +6,10 @@ import (
 	"log"
 	"os"
 	"time"
-	"go.mongodb.org/mongo-driver/mongo/options"
+
 	"github.com/joho/godotenv"
 	"go.mongo.org/mongo-driver/mongo"
-
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 func DBinstance() *mongo.Client{
 	err := godotenv.Load(".env")
@@ -17,5 +17,27 @@ func DBinstance() *mongo.Client{
 		log.Fatal("Eroor loading .env file")
 	}
 
-	
+	MongoDb := os.Getenv("MONGODB_URL")
+	client,err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil{
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(),10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to mongoDB")
+	return client
+
+}
+
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client,collectionName string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("").Collection(collectionName)
+	return collection
 }
